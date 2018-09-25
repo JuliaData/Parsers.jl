25f47410274c1458bbdbddc6252d410a2f045492
quinnj <quinn.jacobd@gmail.com>
2018-09-24 20:29:33 -0600

    Fix ignore_repeated for Dates

--- src/dates.jl
+++ src/dates.jl
@@ -1,24 +1,24 @@
 make(df::Dates.DateFormat) = df
 make(df::String) = Dates.DateFormat(df)
 
-@inline parse!(d::Delimited, io::IO, r::Result{T}; kwargs...) where {T <: Dates.TimeType} =
-    parse!(d.next, io, r, d.delims; kwargs...)
-@inline parse!(q::Quoted, io::IO, r::Result{T}, delims=nothing; kwargs...) where {T <: Dates.TimeType} =
-    parse!(q.next, io, r, delims, q.openquotechar, q.closequotechar, q.escapechar, q.ignore_quoted_whitespace; kwargs...)
-@inline parse!(s::Strip, io::IO, r::Result{T}, delims=nothing, openquotechar=nothing, closequotechar=nothing, escapechar=nothing, ignore_quoted_whitespace=false; kwargs...) where {T <: Dates.TimeType} =
-    parse!(s.next, io, r, delims, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace; kwargs...)
-@inline parse!(s::Sentinel, io::IO, r::Result{T}, delims=nothing, openquotechar=nothing, closequotechar=nothing, escapechar=nothing, ignore_quoted_whitespace=false; kwargs...) where {T <: Dates.TimeType} =
-    parse!(s.next, io, r, delims, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace, s.sentinels; kwargs...)
-@inline parse!(::typeof(defaultparser), io::IO, r::Result{T}, delims=nothing, openquotechar=nothing, closequotechar=nothing, escapechar=nothing, ignore_quoted_whitespace=false, node=nothing; kwargs...) where {T <: Dates.TimeType} =
-    defaultparser(io, r, delims, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace, node; kwargs...)
+@inline parse!(d::Delimited{ignore_repeated}, io::IO, r::Result{T}; kwargs...) where {ignore_repeated, T <: Dates.TimeType} =
+    parse!(d.next, io, r, d.delims, ignore_repeated; kwargs...)
+@inline parse!(q::Quoted, io::IO, r::Result{T}, delims=nothing, ignore_repeated=false; kwargs...) where {T <: Dates.TimeType} =
+    parse!(q.next, io, r, delims, ignore_repeated, q.openquotechar, q.closequotechar, q.escapechar, q.ignore_quoted_whitespace; kwargs...)
+@inline parse!(s::Strip, io::IO, r::Result{T}, delims=nothing, ignore_repeated=false, openquotechar=nothing, closequotechar=nothing, escapechar=nothing, ignore_quoted_whitespace=false; kwargs...) where {T <: Dates.TimeType} =
+    parse!(s.next, io, r, delims, ignore_repeated, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace; kwargs...)
+@inline parse!(s::Sentinel, io::IO, r::Result{T}, delims=nothing, ignore_repeated=false, openquotechar=nothing, closequotechar=nothing, escapechar=nothing, ignore_quoted_whitespace=false; kwargs...) where {T <: Dates.TimeType} =
+    parse!(s.next, io, r, delims, ignore_repeated, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace, s.sentinels; kwargs...)
+@inline parse!(::typeof(defaultparser), io::IO, r::Result{T}, delims=nothing, ignore_repeated=false, openquotechar=nothing, closequotechar=nothing, escapechar=nothing, ignore_quoted_whitespace=false, node=nothing; kwargs...) where {T <: Dates.TimeType} =
+    defaultparser(io, r, delims, ignore_repeated, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace, node; kwargs...)
 
 @inline function defaultparser(io::IO, r::Result{T},
-    delims=nothing, openquotechar=nothing, closequotechar=nothing,
+    delims=nothing, ignore_repeated=false, openquotechar=nothing, closequotechar=nothing,
     escapechar=nothing, ignore_quoted_whitespace=false, node=nothing;
     dateformat::Union{String, Dates.DateFormat}=Dates.default_format(T),
     kwargs...) where {T <: Dates.TimeType}
     setfield!(r, 3, Int64(position(io)))
-    res = defaultparser(io, Result(String), delims, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace, node)
+    res = defaultparser(io, Result(String), delims, ignore_repeated, openquotechar, closequotechar, escapechar, ignore_quoted_whitespace, node)
     setfield!(r, 1, missing)
     code = res.code
     if ok(res.code)
