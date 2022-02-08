290c897d72673fbf2451f058cbf509a8e15908b3
rafaqz <rafaelschouten@gmail.com>
2022-02-08 21:06:38 +0100

    reduce type list and use tryparse instead of xparse2

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,46 +1,25 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{typeof(Parsers.parse), Type{Int64}, String})
-    precompile(Tuple{typeof(Parsers.parse), Type{Float64}, String})
-    precompile(Tuple{typeof(Parsers.parse), Type{Date}, String})
 
     options = Parsers.Options()
     pos = 1
-    val = "a"
-    len = length(val)
-    for T in (Char, String), buf in (codeunits(val), Vector(codeunits(val)))
-        Parsers.xparse(T, buf, pos, len, options)
-        Parsers.xparse(T, buf, pos, len, options, Any)
-    end
-
-    for T in (Char, Symbol), buf in (val, Vector(codeunits(val)))
-        Parsers.xparse2(T, buf, pos, len, options)
-        Parsers.xparse2(T, buf, pos, len, options, T)
-        Parsers.xparse2(T, buf, pos, len, options, Any)
-    end
-
     val = "123"
     len = length(val)
-    for T in (Int8, Int16, Int32, Int64, Float16, Float32, Float64, BigFloat, Dates.Date, Dates.DateTime, Dates.Time, Bool)
+    for T in (String, Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Bool)
         for buf in (codeunits(val), Vector(codeunits(val)))
             Parsers.xparse(T, buf, pos, len, options)
-            Parsers.xparse(T, buf, pos, len, options, T)
+            if !(T === String)
+                Parsers.xparse(T, buf, pos, len, options, T)
+            end
             Parsers.xparse(T, buf, pos, len, options, Any)
         end
     end
-    for T in (Int8, Int16, Int32, Int64, Float16, Float32, Float64, BigFloat, Dates.Date, Dates.DateTime, Dates.Time, Bool)
+
+    for T in (Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Bool)
         for buf in (val, SubString(val, 1:3), Vector(codeunits(val)), view(Vector(codeunits(val)), 1:3))
-            Parsers.xparse2(T, buf, pos, len, options)
-            Parsers.xparse2(T, buf, pos, len, options, T)
-            Parsers.xparse2(T, buf, pos, len, options, Any)
+            Parsers.tryparse(T, buf, options)
         end
     end
-    for T in (Int8, Int16, Int32, Int64, Float16, Float32, Float64, BigFloat, Dates.Date, Dates.DateTime)
-        for buf in (val, SubString(val, 1:3), Vector(codeunits(val)), view(Vector(codeunits(val)), 1:3))
-            Parsers.parse(T, buf)
-            Parsers.parse(T, buf, options)
-            Parsers.tryparse(T, buf)
-        end
-    end
+
 end
 _precompile_()
