29f99859b8c85580c3755bedc9069a2db6b5f8d9
rafaqz <rafaelschouten@gmail.com>
2022-02-09 13:05:13 +0100

    remove 6 arg typed xparse precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -8,9 +8,6 @@ function _precompile_()
     for T in (String, Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Bool)
         for buf in (codeunits(val), Vector(codeunits(val)))
             Parsers.xparse(T, buf, pos, len, options)
-            if !(T === String)
-                Parsers.xparse(T, buf, pos, len, options, T)
-            end
             Parsers.xparse(T, buf, pos, len, options, Any)
         end
     end
