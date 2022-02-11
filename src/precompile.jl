e7fe3e74c1022dbf628a6ef66948b93cc8c4b83d
Rafael Schouten <rafaelschouten@gmail.com>
2022-02-11 01:03:07 +0100

    add precompile runs for xparse and xparse2 (#108)
    * add precompile statements for xparse
    
    * add precompilation for xparse2
    
    * use nested for loops
    
    * reduce type list and use tryparse instead of xparse2
    
    * remove 6 arg typed xparse precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,22 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{typeof(Parsers.parse), Type{Int64}, String})
-    precompile(Tuple{typeof(Parsers.parse), Type{Float64}, String})
-    precompile(Tuple{typeof(Parsers.parse), Type{Date}, String})
+
+    options = Parsers.Options()
+    pos = 1
+    val = "123"
+    len = length(val)
+    for T in (String, Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Bool)
+        for buf in (codeunits(val), Vector(codeunits(val)))
+            Parsers.xparse(T, buf, pos, len, options)
+            Parsers.xparse(T, buf, pos, len, options, Any)
+        end
+    end
+
+    for T in (Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Bool)
+        for buf in (val, SubString(val, 1:3), Vector(codeunits(val)), view(Vector(codeunits(val)), 1:3))
+            Parsers.tryparse(T, buf, options)
+        end
+    end
+
 end
 _precompile_()
