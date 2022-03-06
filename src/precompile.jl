b3e6da8f64f6f598b5bf083f19b18341fecc931a
Jacob Quinn <quinn.jacobd@gmail.com>
2022-03-05 21:18:26 -0700

    Remove Dates-related precompiles
    Diagnosed to be causing CSV.jl precompile to fail on windows for an
    unknown reason (https://github.com/JuliaData/CSV.jl/issues/981).
    Removing the precompiles for now until further investigation can be done
    to figure out the root cause.

--- src/precompile.jl
+++ src/precompile.jl
@@ -5,14 +5,14 @@ function _precompile_()
     pos = 1
     val = "123"
     len = length(val)
-    for T in (String, Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Bool)
+    for T in (String, Int32, Int64, Float64, BigFloat, Bool)
         for buf in (codeunits(val), Vector(codeunits(val)))
             Parsers.xparse(T, buf, pos, len, options)
             Parsers.xparse(T, buf, pos, len, options, Any)
         end
     end
 
-    for T in (Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Bool)
+    for T in (Int32, Int64, Float64, BigFloat, Bool)
         for buf in (val, SubString(val, 1:3), Vector(codeunits(val)), view(Vector(codeunits(val)), 1:3))
             Parsers.tryparse(T, buf, options)
         end
