484c25feaedd26809ea57e8c099b4ea10551faef
Jacob Quinn <quinn.jacobd@gmail.com>
2022-03-28 11:27:21 -0600

    Revert precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,22 +1,7 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-
-    options = Parsers.Options()
-    pos = 1
-    val = "123"
-    len = length(val)
-    for T in (String, Int32, Int64, Float64, BigFloat, Bool)
-        for buf in (codeunits(val), Vector(codeunits(val)))
-            Parsers.xparse(T, buf, pos, len, options)
-            Parsers.xparse(T, buf, pos, len, options, Any)
-        end
-    end
-
-    for T in (Int32, Int64, Float64, BigFloat, Bool)
-        for buf in (val, SubString(val, 1:3), Vector(codeunits(val)), view(Vector(codeunits(val)), 1:3))
-            Parsers.tryparse(T, buf, options)
-        end
-    end
-
+    precompile(Tuple{typeof(Parsers.parse), Type{Int64}, String})
+    precompile(Tuple{typeof(Parsers.parse), Type{Float64}, String})
+    precompile(Tuple{typeof(Parsers.parse), Type{Date}, String})
 end
 _precompile_()
