86f83bbe00324f37a4f9773ea7d332bb46c972ba
Tim Holy <tim.holy@gmail.com>
2023-04-24 16:43:50 -0500

    Migrate from SnoopPrecompile to PrecompileTools

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,13 +1,13 @@
-using SnoopPrecompile
+using PrecompileTools
 
-@precompile_setup begin
+@setup_workload begin
     # Putting some things in `setup` can reduce the size of the
     # precompile file and potentially make loading faster.
     options = Parsers.Options()
     pos = 1
     val = "123"
     len = length(val)
-    @precompile_all_calls begin
+    @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
         for T in (String, Int32, Int64, Float64, BigFloat, Dates.Date, Dates.DateTime, Dates.Time, Bool)
